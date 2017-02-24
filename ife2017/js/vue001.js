2680649f48bbeaf4ce4297fd26087fda826faf99
Felix the guy <fixsmax@live.com>
2017-02-24 15:48:45 +0800

    v6.01

---
+++ ife2017/js/vue001.js
@@ -0,0 +1,14 @@
+/*Created by FTG on 2/23/2017.*/
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!'
+    }
+});
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date()
+    }
+});
---
+++ selfstudy/js/portfolio.js
@@ -0,0 +1,34 @@
+/**
+ * Created by felix on 2/21/2017.
+ */
+$(document).ready(function(){
+    /* affix the navbar after scroll below header */
+    $(".navbar").affix({offset: {top: $("header").outerHeight(true)} });
+});
+
+
+$(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#topNavbar a").on('click', function(event) {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    });
+});
+
